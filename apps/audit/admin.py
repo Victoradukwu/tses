3f53b87a1dbe00b8511dc5c4fe783a60a4a158a7
Victor <vicads01@gmail.com>
2026-02-27 09:18:09 +0100

    Fix location of Audit model

--- apps/audit/admin.py
+++ apps/audit/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Audit
+
+@admin.register(Audit)
+class AuditAdmin(admin.ModelAdmin):
+    list_display = ['event', 'email', 'ip_address', 'user_agent', 'metadata', 'created_at']
+    history_list_display = ['event', 'email', 'ip_address', 'user_agent', 'metadata', 'created_at']
+    search_fields = ['event', 'email']
+    list_filter = ['event']
+
--- apps/audit/models.py
+++ apps/audit/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Audit(models.Model):
+    OTP_VERIFIED = "OTP_VERIFIED"
+    OTP_FAILED = "OTP_FAILED"
+    OTP_LOCKED = "OTP_LOCKED"
+    EVENT_CHOICES = [(OTP_VERIFIED, OTP_VERIFIED), (OTP_FAILED, OTP_FAILED), (OTP_LOCKED, OTP_LOCKED)]
+
+    event = models.CharField(max_length=12, choices=EVENT_CHOICES, default=OTP_VERIFIED)
+    email = models.EmailField()
+    ip_address = models.GenericIPAddressField()
+    user_agent = models.CharField(max_length=255, null=True, blank=True)
+    metadata = models.JSONField()
+    created_at = models.DateTimeField(auto_now_add=True)
